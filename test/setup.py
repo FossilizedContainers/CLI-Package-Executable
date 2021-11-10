ad9278e9266dd83e64bf82a6acfda157f442efe4
JeremyKlein21 <77638390+JeremyKlein21@users.noreply.github.com>
2021-11-10 12:42:23 -0700

    Add files via upload

---
+++ test/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='clickLibrary',
+    version='1',
+    packages=[''],
+    url='',
+    license='',
+    author='Jeremy Klein',
+    author_email='jmk547@nau.edu',
+    description='Test Python package '
+)
